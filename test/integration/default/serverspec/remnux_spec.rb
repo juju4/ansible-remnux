2d47597e3921c4d88fa3d1cf30f5f389076b0c6e
juju4 <juju4@users.noreply.github.com>
2016-08-04 22:35:21 -0400

    sync repositories

--- test/integration/default/serverspec/remnux_spec.rb
+++ test/integration/default/serverspec/remnux_spec.rb
@@ -3,34 +3,34 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe service('snort') do
-  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('argus') do
+#describe service('snort') do
 #  it { should be_enabled }
-  it { should be_running }
-end
-
-describe service('samba') do
-#  it { should be_enabled }
-  it { should be_running }
-end
+#  it { should be_running }
+#end
+#
+#describe service('argus') do
+##  it { should be_enabled }
+#  it { should be_running }
+#end
+#
+#describe service('samba') do
+##  it { should be_enabled }
+#  it { should be_running }
+#end
 
 describe port(22) do
   it { should be_listening }
 end
 
-describe port(445) do
-  it { should be_listening }
-end
+#describe port(445) do
+#  it { should be_listening }
+#end
 
 describe file('/usr/share/remnux') do
   it { should be_directory }
 end
 
-describe file('/home/vagrant/Desktop/REMnux Tools Sheet') do
+describe file('/home/remnux/Desktop/REMnux Tools Sheet') do
   it { should be_symlink }
 end
 
