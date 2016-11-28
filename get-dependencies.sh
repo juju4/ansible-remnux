7e6261a444a57ed8ede345ba034138273df249c6
juju4 <juju4@users.noreply.github.com>
2016-11-27 19:04:01 -0500

    sync repositories

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,4 +10,5 @@ rolesdir=$(dirname $0)/..
 fi
 
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
+[ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
 
