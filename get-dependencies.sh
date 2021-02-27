6600b16e7d726c1a15323a7f140ed0db66cdb7d7
juju4 <juju4@users.noreply.github.com>
2021-02-27 18:15:59 -0500

    user geerlingguy.docker role

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -11,13 +11,13 @@ fi
 
 #[ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/geerlingguy/ansible-role-java.git $rolesdir/geerlingguy.java
 [ ! -d $rolesdir/geerlingguy.java ] && git clone https://github.com/juju4/ansible-role-java.git $rolesdir/geerlingguy.java
+[ ! -d $rolesdir/geerlingguy.docker ] && git clone https://github.com/geerlingguy/ansible-role-docker.git $rolesdir/geerlingguy.docker
 #[ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
 #[ ! -d $rolesdir/jgeusebroek.docker ] && git clone https://github.com/jgeusebroek/ansible-role-docker.git $rolesdir/jgeusebroek.docker
-[ ! -d $rolesdir/jgeusebroek.docker ] && git clone https://github.com/juju4/ansible-role-docker.git $rolesdir/jgeusebroek.docker
+#[ ! -d $rolesdir/jgeusebroek.docker ] && git clone https://github.com/juju4/ansible-role-docker.git $rolesdir/jgeusebroek.docker
 ## galaxy naming: kitchen fails to transfer symlink folder
 #[ ! -e $rolesdir/juju4.remnux ] && ln -s ansible-remnux $rolesdir/juju4.remnux
 [ ! -e $rolesdir/juju4.remnux ] && cp -R $rolesdir/ansible-remnux $rolesdir/juju4.remnux
 
 ## don't stop build on this script return code
 true
-
